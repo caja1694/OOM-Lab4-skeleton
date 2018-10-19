c5070a268ecd402527a2de16076e37c1e4bd6b0a
caja1694 <43992994+caja1694@users.noreply.github.com>
2018-10-19 14:01:57 +0200

    Update Test/se/ju/csi/oom/lab4/DateTimeTest.java

---
+++ Test/se/ju/csi/oom/lab4/DateTimeTest.java
@@ -0,0 +1,27 @@
+package se.ju.csi.oom.lab4;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DateTimeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void toStringTest() {
+		DateTime testDateTime = new DateTime(1994, 01, 29, 10, 10, 10);
+		assertEquals(testDateTime.toString(), "1994-01-29 10:10:10");
+	}
+	@Test
+	public void dateTimeFromStringTest(){
+		DateTime testDateTime = new DateTime("1994-01-29 10:10:10");
+		DateTime compareDateTime = new DateTime(1994, 01, 29, 10, 10, 10);
+		assertEquals(testDateTime.toString(), compareDateTime.toString());
+		
+	}
+
+}
